a94f6a57efc33623adc3cba29f5338288f96b0e4
Sean Edwards <seantrevoredwards@gmail.com>
2021-01-26 10:20:26 +0000

    started and setup birthday app passed first test for welcoming users

---
+++ spec/features/birthday_homepage_spec.rb
@@ -0,0 +1,11 @@
+feature 'Homepage welcomes users' do
+  scenario 'it says hello when you visit the page' do
+    visit('/')
+    expect(page).to have_content("Hello There")
+  end
+
+  scenario 'it asks for your name' do
+    visit ('/')
+    expect(page.to have_content("What's your name?"))
+  end
+end
